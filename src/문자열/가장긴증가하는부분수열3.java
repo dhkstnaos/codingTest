80dafaec4939f3e5a5ae2d31bd1a5b29fb90f6a6
dhkstnaos <dhkstnaos@gmail.com>
2022-01-21 14:04:39 +0900

    Add 문자열

---
+++ src/문자열/가장긴증가하는부분수열3.java
@@ -0,0 +1,39 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 가장긴증가하는부분수열3 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(Integer.MIN_VALUE); //초기 값은 그냥 넣는다.
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<n;i++) {
+            int num= Integer.parseInt(st.nextToken());
+            if(num>list.get(list.size()-1)) { //list 끝 값보다 크다면 list에 추가
+                list.add(num);
+            }
+            else { //list 안에서 적절한 위치를 찾는다.
+                int left=0;
+                int right=list.size()-1;
+                while(left<right) {
+                    int mid=(left+right)/2;
+                    if(list.get(mid)<num) {
+                        left=mid+1;
+                    }
+                    else {
+                        right=mid;
+                    }
+                }
+                list.set(right,num);
+            }
+        }
+        System.out.println(list.size()-1);
+    }
+}
