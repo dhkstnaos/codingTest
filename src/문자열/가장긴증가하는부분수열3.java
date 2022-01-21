ebe542cdf438d1e8763ac8cbca9bc0ea033766b8
dhkstnaos <dhkstnaos@gmail.com>
2022-01-21 16:07:17 +0900

    Add dfs

--- src/문자열/가장긴증가하는부분수열3.java
+++ src/문자열/가장긴증가하는부분수열3.java
@@ -6,7 +6,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-
+/*
+* https://www.acmicpc.net/problem/12738
+* 예제 입력
+6
+10 20 10 30 20 50
+* 예제 출력
+4
+* */
 public class 가장긴증가하는부분수열3 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
